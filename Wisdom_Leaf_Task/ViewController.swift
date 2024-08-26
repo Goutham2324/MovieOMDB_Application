e222750917482412cb58866d9f20f41f41b72e16
Swejan Kothamasu <swejan@deskfactors.com>
2024-08-26 21:46:39 +0530

    Initial Commit

---
+++ Wisdom_Leaf_Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wisdom_Leaf_Task
+//
+//  Created by Swejan Kothamasu on 26/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Wisdom_Leaf_TaskUITests/Wisdom_Leaf_TaskUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Wisdom_Leaf_TaskUITestsLaunchTests.swift
+//  Wisdom_Leaf_TaskUITests
+//
+//  Created by Swejan Kothamasu on 26/08/24.
+//
+
+import XCTest
+
+final class Wisdom_Leaf_TaskUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
